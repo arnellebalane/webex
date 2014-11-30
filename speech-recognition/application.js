bf17f9a5a941b1a5f2ea88530633baf549b70176
Arnelle Balane <arnellebalane@gmail.com>
2014-11-30 09:24:52 +0800

    added interface to speech recognition experiment

--- speech-recognition/application.js
+++ speech-recognition/application.js
@@ -1,32 +1,79 @@
-var recognizer = new webkitSpeechRecognition();
+var icon = document.querySelector('.indicator');
+var text = document.querySelector('h1');
+var processing = false;
 
-recognizer.onstart = function(e) {
-    console.log('Starting Speech Recognition, please say something...');
-};
-
-recognizer.onend = function(e) {
-    console.log('Speech Recognition session has ended...');
-};
-
-recognizer.onspeechstart = function(e) {
-    console.log('Speech detected. Capturing...');
-};
-
-recognizer.onspeechend = function(e) {
-    console.log('Speech captured. Processing...');
-};
-
-recognizer.onnomatch = function(e) {
-    console.error('No match found. Please speak properly.');
-};
-
-recognizer.onresult = function(e) {
-    for (var i = 0; i < e.results.length; i++) {
-        var result = e.results[i];
-        for (var j = 0; j < result.length; j++) {
-            var alternative = result[j];
-            console.info(alternative.transcript + ' (' + alternative.confidence + ')');
-        }
+icon.addEventListener('click', function() {
+    if (this.classList.contains('idle')) {
+        start_recognition();
     }
-};
-recognizer.start();
+});
+
+function start_recognition() {
+    var recognizer = new webkitSpeechRecognition();
+
+    recognizer.onstart = onstart;
+    recognizer.onend = onend;
+    recognizer.onspeechstart = onspeechstart;
+    recognizer.onspeechend = onspeechend;
+    recognizer.onnomatch = onnomatch;
+    recognizer.onerror = onerror;
+    recognizer.onresult = onresult;
+
+    recognizer.start();
+}
+
+function onstart(e) {
+    icon.classList.remove('idle');
+    icon.classList.add('recording');
+    log('You may say something now.');
+}
+
+function onend(e) {
+    icon.classList.remove('recording');
+    icon.classList.remove('processing');
+    icon.classList.add('idle');
+    if (processing) {
+        processing = false;
+        onnomatch();
+    }
+}
+
+function onspeechstart(e) {
+    log('Speech detected. Capturing...');
+}
+
+function onspeechend(e) {
+    icon.classList.remove('recording');
+    icon.classList.add('processing');
+    processing = true;
+    log('Speech captured. Processing...');
+}
+
+function onnomatch(e) {
+    onend();
+    log('No match found. Please speak properly.');
+}
+
+function onerror(e) {
+    if (e.error === 'not-allowed') {
+        onend();
+        log('Please allow the page to use the microphone.');
+    } else {
+        onnomatch();
+    }
+}
+
+function onresult(e) {
+    if (e.results.length) {
+        var result = e.results[0][0];
+        processing = false;
+        onend();
+        log('"' + result.transcript + '"');
+    } else {
+        onnomatch();
+    }
+}
+
+function log(message) {
+    text.textContent = message;
+}
